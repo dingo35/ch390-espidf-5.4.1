08fc7d6bcdf5357f1c76e03d310b5063e58d806b
dingo35 <spaarder@hotmail.nl>
2025-06-04 07:14:56 +0200

    main.cpp: include simple_ethernet stuff

--- main/main.cpp
+++ main/main.cpp
@@ -1,10 +1,148 @@
 #include "Arduino.h"
+/*
+#include "esp_eth_phy_ch390.h"
+#include "esp_eth_mac_ch390.h"
+#define CONFIG_TCPSERVER_ETH_SPI_CLOCK_MHZ 10
+#define CONFIG_TCPSERVER_ETH_SPI_CS_GPIO 5
+#define CONFIG_TCPSERVER_ETH_SPI_HOST 1
+
+// Configure SPI interface for specific SPI module
+spi_device_interface_config_t spi_devcfg = {
+    .mode = 0,
+    .clock_speed_hz = CONFIG_TCPSERVER_ETH_SPI_CLOCK_MHZ * 1000 * 1000,
+    .queue_size = 16,
+    .spics_io_num = CONFIG_TCPSERVER_ETH_SPI_CS_GPIO
+};
+
+eth_ch390_config_t ch390_config = ETH_CH390_DEFAULT_CONFIG(CONFIG_TCPSERVER_ETH_SPI_HOST,&spi_devcfg);
+ch390_config.int_gpio_num = CONFIG_TCPSERVER_ETH_SPI_INT_GPIO;
+
+#if CONFIG_TCPSERVER_ETH_SPI_INT_GPIO < 0
+ch390_config.poll_period_ms = CONFIG_TCPSERVER_ETH_SPI_POLLING_MS_VAL;
+#endif
+
+eth_mac_config_t mac_config = ETH_MAC_DEFAULT_CONFIG();
+
+// To avoid stack overflow, you could choose a larger value
+mac_config.rx_task_stack_size = 4096;
+
+// CH390 has a factory burned MAC. Therefore, configuring MAC address manually is optional.     
+esp_eth_mac_t *mac = esp_eth_mac_new_ch390(&ch390_config,&mac_config);
+
+
+eth_phy_config_t phy_config = ETH_PHY_DEFAULT_CONFIG();
+
+esp_eth_phy_t *phy = esp_eth_phy_new_ch390(&phy_config);
 
 void setup() {
   Serial.begin(115200);
 }
 
+*/
 void loop() {
   Serial.println("Hello world!");
   delay(1000);
+}
+/*
+ * SPDX-FileCopyrightText: 2023 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#include <stdio.h>
+#include <string.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_netif.h"
+#include "esp_eth.h"
+#include "esp_event.h"
+#include "esp_log.h"
+#include "ethernet_init.h"
+#include "sdkconfig.h"
+
+static const char *TAG = "ethernet_basic";
+
+
+/* Event handler for IP_EVENT_ETH_GOT_IP */
+static void got_ip_event_handler(void *arg, esp_event_base_t event_base,
+                                 int32_t event_id, void *event_data)
+{
+    ip_event_got_ip_t *event = (ip_event_got_ip_t *) event_data;
+    const esp_netif_ip_info_t *ip_info = &event->ip_info;
+
+    ESP_LOGI(TAG, "Ethernet Got IP Address");
+    ESP_LOGI(TAG, "~~~~~~~~~~~");
+    ESP_LOGI(TAG, "IP: " IPSTR, IP2STR(&ip_info->ip));
+    ESP_LOGI(TAG, "MASK: " IPSTR, IP2STR(&ip_info->netmask));
+    ESP_LOGI(TAG, "GW: " IPSTR, IP2STR(&ip_info->gw));
+    ESP_LOGI(TAG, "~~~~~~~~~~~");
+}
+
+//void app_main(void)
+void setup(void)
+{
+    uint8_t eth_port_cnt = 0;
+    esp_eth_handle_t *eth_handles;
+    char if_key_str[10];
+    char if_desc_str[10];
+
+    // Initialize TCP/IP network interface aka the esp-netif (should be called only once in application)
+    ESP_ERROR_CHECK(esp_netif_init());
+    // Create default event loop that running in background
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+
+    // Initialize Ethernet driver
+    ESP_ERROR_CHECK(ethernet_init_all(&eth_handles, &eth_port_cnt));
+
+    // Create instance(s) of esp-netif for Ethernet(s)
+    if (eth_port_cnt == 1) {
+        // Use ESP_NETIF_DEFAULT_ETH when just one Ethernet interface is used and you don't need to modify
+        // default esp-netif configuration parameters.
+        esp_netif_config_t cfg = ESP_NETIF_DEFAULT_ETH();
+        esp_netif_t *eth_netif = esp_netif_new(&cfg);
+        // Attach Ethernet driver to TCP/IP stack
+        ESP_ERROR_CHECK(esp_netif_attach(eth_netif, esp_eth_new_netif_glue(eth_handles[0])));
+    } else {
+        // Use ESP_NETIF_INHERENT_DEFAULT_ETH when multiple Ethernet interfaces are used and so you need to modify
+        // esp-netif configuration parameters for each interface (name, priority, etc.).
+        esp_netif_inherent_config_t esp_netif_config = ESP_NETIF_INHERENT_DEFAULT_ETH();
+        esp_netif_config_t cfg_spi = {
+            .base = &esp_netif_config,
+            .stack = ESP_NETIF_NETSTACK_DEFAULT_ETH
+        };
+
+        for (int i = 0; i < eth_port_cnt; i++) {
+            sprintf(if_key_str, "ETH_%d", i);
+            sprintf(if_desc_str, "eth%d", i);
+            esp_netif_config.if_key = if_key_str;
+            esp_netif_config.if_desc = if_desc_str;
+            esp_netif_config.route_prio -= i * 5;
+            esp_netif_t *eth_netif = esp_netif_new(&cfg_spi);
+
+            // Attach Ethernet driver to TCP/IP stack
+            ESP_ERROR_CHECK(esp_netif_attach(eth_netif, esp_eth_new_netif_glue(eth_handles[i])));
+        }
+    }
+
+    // Register user defined event handers
+    ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_ETH_GOT_IP, &got_ip_event_handler, NULL));
+
+    // Start Ethernet driver state machine
+    for (int i = 0; i < eth_port_cnt; i++) {
+        ESP_ERROR_CHECK(esp_eth_start(eth_handles[i]));
+    }
+
+    // Print each device info
+    for (int i = 0; i < eth_port_cnt; i++) {
+        eth_dev_info_t info = ethernet_init_get_dev_info((void**) eth_handles[i]);
+        //eth_dev_info_t info = ethernet_init_get_dev_info(eth_handles[i]);
+        if (info.type == ETH_DEV_TYPE_INTERNAL_ETH) {
+            ESP_LOGI(TAG, "Device Name: %s", info.name);
+            ESP_LOGI(TAG, "Device type: ETH_DEV_TYPE_INTERNAL_ETH(%d)", info.type);
+            ESP_LOGI(TAG, "Pins: mdc: %d, mdio: %d", info.pin.eth_internal_mdc, info.pin.eth_internal_mdio);
+        } else if (info.type == ETH_DEV_TYPE_SPI) {
+            ESP_LOGI(TAG, "Device Name: %s", info.name);
+            ESP_LOGI(TAG, "Device type: ETH_DEV_TYPE_SPI(%d)", info.type);
+            ESP_LOGI(TAG, "Pins: cs: %d, intr: %d", info.pin.eth_spi_cs, info.pin.eth_spi_int);
+        }
+    }
 }
